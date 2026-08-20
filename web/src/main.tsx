b435d377dd04810746b5923a7dab67175c8cb663
jessen reinhart <jessen_1206@yahoo.com>
2026-08-21 06:56:55 +0700

    feat(web): mount setup controls

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,8 +1,9 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import { ContextScopeFeature } from "./ContextScopeFeature";
+import { SetupChrome } from "./SetupChrome";
 import "./index.css";
 
 const root = document.getElementById("root");
 if (!root) throw new Error("root element missing");
-createRoot(root).render(<><App /><ContextScopeFeature /></>);
+createRoot(root).render(<><App /><ContextScopeFeature /><SetupChrome /></>);
