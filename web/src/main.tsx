d3c41221b742dd98cf835228f3b1edb5c4c6618d
jessen reinhart <jessen_1206@yahoo.com>
2026-08-21 17:08:30 +0700

    feat(web): add durable Decision Library

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,9 +3,10 @@ import { App } from "./App";
 import { ContextScopeFeature } from "./ContextScopeFeature";
 import { CanvasNodeDeleteFeature } from "./CanvasNodeDeleteFeature";
 import { SetupChrome } from "./SetupChrome";
+import { DecisionLibraryFeature } from "./DecisionLibraryFeature";
 import "./index.css";
 import "./visual-audit.css";
 
 const root = document.getElementById("root");
 if (!root) throw new Error("root element missing");
-createRoot(root).render(<><App /><ContextScopeFeature /><CanvasNodeDeleteFeature /><SetupChrome /></>);
+createRoot(root).render(<><App /><ContextScopeFeature /><CanvasNodeDeleteFeature /><SetupChrome /><DecisionLibraryFeature /></>);
