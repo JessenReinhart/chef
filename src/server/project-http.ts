8c88356576d61c2bd0698ecb73b21e638f912992
jessen reinhart <jessen_1206@yahoo.com>
2026-09-06 21:29:41 +0700

    Block legacy approval accept during project handoff

--- src/server/project-http.ts
+++ src/server/project-http.ts
@@ -178,6 +178,7 @@ function startsProjectWork(method: string | undefined, pathname: string): boolea
     || /^\/api\/threads\/[^/]+\/chat$/.test(pathname)
     || /^\/api\/nodes\/[^/]+\/retry$/.test(pathname)
     || /^\/api\/tools\/approvals\/[^/]+\/accept$/.test(pathname)
+    || /^\/api\/approvals\/[^/]+\/accept$/.test(pathname)
     || /^\/api\/missions\/[^/]+\/resume$/.test(pathname);
 }
 
